4467c81b3fa4ecf92436bc1dd4f47063838a3b53
dxarch <emilepatch@gmail.com>
2022-10-08 18:41:02 +0300

    Add 7kyu tasks

---
+++ 7kyu/find-missing-element.js
@@ -0,0 +1,21 @@
+// Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, find the missing element.
+
+// Please note, there may be duplicates in the arrays, so checking if a numerical value exists in one and not the other is not a valid solution.
+
+// find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+// find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+// The first array will always have at least one element.
+
+function findMissing(arr1, arr2) {
+    let toFind = null;
+    
+    for (let i = 0; i < arr1.length; ++i) {
+      toFind = arr1[i];
+      const foundedIndex = arr2.indexOf(toFind);
+      
+      if (foundedIndex > -1) arr2.splice(foundedIndex, 1);
+      else break;
+    }
+    
+    return toFind;
+  }
