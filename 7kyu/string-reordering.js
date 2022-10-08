e5ef76a5028ed753017348459c2ad2d1972d50a2
dxarch <emilepatch@gmail.com>
2022-10-08 18:23:56 +0300

    Add 7kyu tasks

---
+++ 7kyu/string-reordering.js
@@ -0,0 +1,31 @@
+// The input will be an array of dictionaries.
+
+// Return the values as a string-seperated sentence in the order of their keys' integer equivalent (increasing order).
+
+// The keys are not reoccurring and their range is -999 < key < 999. The dictionaries' keys & values will always be strings and will always not be empty.
+
+// Example
+// Input:
+// List = [
+//         {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
+//         {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
+//        ]
+
+// Output:
+// 'Vatsan took his dog for a spin'
+
+
+function sentence(List) {
+    const fullObj = Object.assign({}, ...List);
+    
+    const allKeys = Object.keys(fullObj);
+  
+    const sentence = allKeys
+    .sort((a, b) => parseInt(a) - parseInt(b))
+    .reduce(function(prevValue, currKey){
+      return prevValue + " " + fullObj[currKey];
+    }, "")
+    .trim();
+  
+    return sentence;
+  }
